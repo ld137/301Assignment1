df0061b0dedd3d4fa20085406cf37487178a7fe1
ld137 <83894641+ld137@users.noreply.github.com>
2023-03-24 17:19:59 +1300

    Add files via upload
    DistributeRuns.java successfully will move two files to merge then a single file every other time.

--- DistributeRuns.java
+++ DistributeRuns.java
@@ -1,35 +1,39 @@
 import java.io.File;
 
 public class DistributeRuns {
+    //For some reason this is not working so for now I will hardcode it
+    public File originalFolder;
+    public File targetFolder;
+    public int input;
 
-    public static void DistributeRuns (int input){
+    public DistributeRuns(int input, String originalFolderPath, String targetFolderPath) {
+        this.originalFolder = new File(originalFolderPath);
+        if(!originalFolder.exists()){
+            System.err.println("Folder does not exist: " + originalFolderPath);
+        }
+        this.targetFolder = new File(targetFolderPath);
+        if(!targetFolder.exists()){
+            System.err.println("Folder does not exist: " + targetFolderPath);
+        }
+        this.input = input;
+    }
+
+    //public void distribute (int input){
+    public void Distribute() {
+
         if (input > 1) {
             
-            // Create a File object for the folder
-            File originFolder = new File("Temp");
-            if(originFolder.exists()){
-                System.out.println("true");
+            // Get all the files in the original folder
+            File[] files = originalFolder.listFiles();
+            
+            // Move first file to the target folder if targer folder is empty
+            if(targetFolder.listFiles().length == 0)
+            {
+                files[0].renameTo(new File(targetFolder.getAbsolutePath() + files[0].separator + files[0].getName()));
             }
 
-            File targetFolder = new File("Merge");
-            if(originFolder.exists()){
-                System.out.println("true");
-            }
-            
-            // Get all the files in the folder
-            File[] files = originFolder.listFiles();
-            
-            files[0].renameTo(new File(targetFolder.getAbsolutePath() + files[0].separator + files[0].getName()));
-
-            // Loop through each file in the folder
-            for (File file : files) {
-                if (file.isFile()) {
-                    // File targetFile = new File(targetFolder.getAbsolutePath() + file.separator + file.getName());
-                    // // Do something with the file
-                    // file.renameTo(targetFile);
-                    file.renameTo(new File(targetFolder.getAbsolutePath() + file.separator + file.getName()));
-                }
-            }
+            // Move the second file to the target folder
+            files[1].renameTo(new File(targetFolder.getAbsolutePath() + files[1].separator + files[1].getName()));
             return;
         }
         else{
